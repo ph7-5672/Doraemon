2221bb9a5027ad7dfc1ded0bac7a9722e2a5bc72
ph7 <h7peng@163.com>
2020-07-05 06:44:01 +0800

    四次元口袋补充

--- src/main/java/org/ph7/doraemon/handler/CommonEventHandler.java
+++ src/main/java/org/ph7/doraemon/handler/CommonEventHandler.java
@@ -1,13 +1,25 @@
 package org.ph7.doraemon.handler;
 
-import net.minecraft.world.World;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import org.ph7.doraemon.common.Reference;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class CommonEventHandler
 {
+
+    @SubscribeEvent
+    public void playerLogin(PlayerEvent.PlayerLoggedInEvent event)
+    {
+        /*if (event.player instanceof EntityPlayerMP)
+        {
+            EntityPlayerMP player = (EntityPlayerMP) event.player;
+            long lastActiveTime = player.addStat();
+            //player.inventory.setInventorySlotContents();
+        }*/
+    }
 
 }
